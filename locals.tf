0a90bb08ac0be55c29ccd8ce8a040222a0b3cd3e
Rajeshreddyk <rajeshreddy.k.315@gmail.com>
2024-09-26 19:32:44 +0530

    terraform

---
+++ locals.tf
@@ -0,0 +1,5 @@
+locals {
+    # intercollation or concatination
+    resource_name = "${var.project_name}-${var.environment}"
+    az_names = slice(data.aws_availability_zones.available.names, 0, 2)
+}
